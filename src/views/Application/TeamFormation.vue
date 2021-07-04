ad5caa6b5c74ccb36f1dc7c8e3c12c72077c7369
Henry T <henry@henrytu.me>
2021-07-04 16:23:52 -0400

    Join and create team

--- src/views/Application/TeamFormation.vue
+++ src/views/Application/TeamFormation.vue
@@ -1,7 +1,6 @@
 <template>
   <FormSection class="team-formation" label='Team Formation'>
     <div v-if="code">
-
       <Typography type='heading3' as='h3' align='center' color='dark-navy'>
         Team Code
       </Typography>
@@ -22,15 +21,66 @@
 
       <hr class="team-formation__hr">
 
+      <div class="team-formation__buttons">
+        <Button @click="triggerLeaveTeam">
+          Leave Team
+        </Button>
 
+        <Button>
+          Continue
+        </Button>
+      </div>
     </div>
     <div v-else>
-      <Typography type='heading2' align='center' color='dark-navy'>
-        You are currently not on a team.
-      </Typography>
-      <Typography type='heading4' as='p' align='center' color='black'>
-        Registering as a team helps speed up the review process.
-      </Typography>
+
+      <div v-if="joinTeamPage">
+        <Typography type='heading2' align='left' color='dark-navy'>
+          Join Team
+        </Typography>
+        <Typography type='heading4' as='p' align='left' color='black'>
+          Already have a Team Code? Enter it below to join!
+        </Typography>
+        <br />
+
+        <div class="team-formation__join-box">
+          <Input
+              label='Team Code'
+              placeholder='Enter team code'
+              v-model='temporaryCode'
+              name='code'
+              required
+          />
+          <Button @click="triggerJoinTeam">
+            Join
+          </Button>
+        </div>
+
+        <hr class="team-formation__hr">
+
+        <Button @click="joinTeamPage = false">
+          Back
+        </Button>
+      </div>
+      <div v-else>
+        <Typography type='heading2' align='center' color='dark-navy'>
+          You are currently not on a team.
+        </Typography>
+        <Typography type='heading4' as='p' align='center' color='black'>
+          Registering as a team helps speed up the review process.
+        </Typography>
+
+        <hr class="team-formation__hr">
+
+        <div class="team-formation__buttons">
+          <Button @click="triggerCreateTeam">
+            Create Team
+          </Button>
+          <Button @click="joinTeamPage = true">
+            Join Team
+          </Button>
+        </div>
+
+      </div>
     </div>
   </FormSection>
 </template>
@@ -39,17 +89,28 @@
 import useFormSection from '@/utils/useFormSection';
 import FormSection from '@/components/FormSection';
 import Typography from '@/components/Typography';
+import Button from "@/components/Button";
+import Input from '@/components/Input';
+import { createTeam, joinTeam, leaveTeam } from "../../utils/api";
 
 export default {
   name: 'TeamFormation',
   components: {
+    Button,
     FormSection,
     Typography,
+    Input
   },
   props: {
     form: Object,
   },
   emits: ['update:form'],
+  data() {
+    return {
+      joinTeamPage: false,
+      temporaryCode: ''
+    }
+  },
   setup(props) {
     return {
       ...useFormSection(props, {
@@ -58,10 +119,35 @@ export default {
       }),
     };
   },
+  methods: {
+    async triggerLeaveTeam() {
+      await leaveTeam();
+
+      this.code = '';
+      this.memberNames = [];
+    },
+    async triggerCreateTeam() {
+      const newTeam = await createTeam();
+
+      this.code = newTeam.data.code;
+      this.memberNames = newTeam.data.memberNames;
+    },
+    async triggerJoinTeam() {
+      const newTeam = await joinTeam(this.temporaryCode);
+
+      if (newTeam.success) {
+        this.code = newTeam.data.code;
+        this.memberNames = newTeam.data.memberNames;
+      } else {
+        alert(newTeam.data);
+      }
+    }
+  }
 };
 </script>
 
 <style lang="scss">
+  @use '@/styles/mixins';
   @use '@/styles/units';
 
   .team-formation {
@@ -69,5 +155,26 @@ export default {
       margin-top: units.spacing(6);
       margin-bottom: units.spacing(6);
     }
+
+    &__buttons {
+      grid-template-columns: 1fr 1fr;
+      grid-gap: units.spacing(6);
+      display: grid;
+
+      @include mixins.media(tablet) {
+        grid-template-columns: 1fr;
+      }
+    }
+
+    &__join-box {
+      grid-template-columns: 8fr 2fr;
+      grid-gap: units.spacing(6);
+      display: grid;
+
+      @include mixins.media(tablet) {
+        grid-template-columns: 1fr;
+      }
+    }
   }
 </style>
+
