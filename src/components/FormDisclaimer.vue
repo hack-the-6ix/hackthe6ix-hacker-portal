c91348c5b64bf7263cf3b5acdc5cfe9b32c2e8bc
JennyVong <jennyvong502@gmail.com>
2021-07-09 03:08:41 -0400

    form-disclaimer and message modal

--- src/components/FormDisclaimer.vue
+++ src/components/FormDisclaimer.vue
@@ -6,39 +6,77 @@
       'form-disclaimer',
     ]"
   >
-    <Close />
+    <button @click="$emit('close')" class="form-disclaimer__close" type="button">
+      <Close />
+    </button>
 
-    <div class="form-disclaimer__content">
-      <SuccessIcon class="form-disclaimer__success-icon" />
-      <ErrorIcon v-if="error" class="form-disclaimer__error-icon" />
-      <DisableIcon v-if="disabled" class="form-disclaimer__disable-icon" />
+    <div class="form-disclaimer__body">
+      <slot name="body">
 
-      <Typography
-        class="form-disclaimer__label"
-        color="black"
-        type="heading4"
-        as="label"
-      >
-        All the fields have been filled in correctly
-      </Typography>
-      <Typography
-        v-if="error"
-        class="form-disclaimer__label"
-        color="black"
-        type="heading4"
-        as="label"
-      >
-        Please resolve the following before proceeding on to the next page. 
-      </Typography>
-      <Typography
-        v-if="disabled"
-        class="form-disclaimer__label"
-        color="black"
-        type="heading4"
-        as="label"
-      >
-        Please complete the previous section to edit this page. 
-      </Typography>
+        <SuccessIcon class="form-disclaimer__icon" />
+        <ErrorIcon v-if="error" class="form-disclaimer__icon" />
+        <DisableIcon v-if="disabled" class="form-disclaimer__icon" />
+
+        <div class="form-disclaimer__details">
+          <Typography
+            class="form-disclaimer__label"
+            color="black"
+            type="heading4"
+            as="label"
+          >
+            All the fields have been filled in correctly
+          </Typography>
+        </div>
+        <div class="form-disclaimer__details">
+          <Typography
+            v-if="error"
+            class="form-disclaimer__label"
+            color="black"
+            type="heading4"
+            as="label"
+          >
+            Please resolve the following before proceeding on to the next page. 
+          </Typography>
+          <ul
+            v-if="error" 
+            class="form-disclaimer__error"
+          >
+          <Typography
+            class="form-disclaimer__points"
+            color="error"
+            type="heading4"
+            as="a"
+          >
+            <li><a href="#">About You</a></li>
+          </Typography>
+          </ul>
+        </div>
+        <div class="form-disclaimer__details">
+          <Typography
+            v-if="disabled"
+            class="form-disclaimer__label"
+            color="black"
+            type="heading4"
+            as="a"
+          >
+            Please complete the previous section to edit this page. 
+          </Typography>
+          <ul 
+            v-if="disabled"
+            class="form-disclaimer__disabled"
+          >
+            <Typography
+              v-if="error"
+              class="form-disclaimer__points"
+              color="navy"
+              type="heading4"
+              as="label"
+            >
+              <li><a href="#">About You</a></li>
+            </Typography>
+          </ul>
+        </div>
+      </slot>
     </div>
   </div>
 </template>
@@ -59,6 +97,8 @@ export default {
     Close
   },
   props: {
+    disabled: Boolean,
+    error: Boolean,
     type: {
       type: String,
       required: true,
@@ -74,7 +114,9 @@ export default {
 .form-disclaimer {
   --form-disclaimer__color: #{colors.css-color(success)};
   --form-disclaimer__background: #{colors.css-color(success, $alpha: 0.15)};
-  display: block;
+  display: grid;
+  grid-template-columns: 1fr;
+  position: relative;
 
   &--error {
     --form-disclaimer__color: #{colors.css-color(error)};
@@ -86,12 +128,38 @@ export default {
     --form-disclaimer__background: #{colors.css-color(disabled, $alpha: 0.15)};
   }
 
-  &__label {
-    color: var(--form-disclaimer__color);
+  &__close {
+    position: absolute;
+    right: 0;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
   }
 
-  border: 1px solid var(--form-disclaimer__color);
-  background-color: var(--form-disclaimer__background);
+  &__body {
+    display: grid;
+    grid-template-columns: 20% 80%;
+    grid-gap: units.spacing(2);
+    padding-top: units.spacing(10);
+    margin-right: units.spacing(10);
+    padding-bottom: units.spacing(10);
+  }
+
+  &__icon {
+    padding-left: units.spacing(6);
+  }
+
+  &__label {
+    color: colors.css-color(black);
+  }
+
+  &__error, &__disabled {
+    list-style-position: inside;
+    padding-left: 0;
+  }
+
+  border-left: 4px solid var(--form-disclaimer__color);
+  background-color: var(--form-disclaimer__background); 
 }
 
 </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class='message'>
+    <button @click="$emit('close')" class="message__close" type="button">
+          <Close />
+        </button>
+
+    <div class="message__body">
+      <slot name="body">
+          <Typography
+            class="message__label"
+            color="black"
+            type="paragraph"
+            as="p"
+          >
+            This question allows us to gauge our
+            hackathon's commitment to diversity
+            and inclusion where we can equitably
+            work towards a better and inclusive
+            hacking community for everyone. We at
+            Hack the 6ix believe that diversity
+            delivers a unique array of ideas and is a
+            key ingredient for better
+            decision-making among teams. 
+          </Typography>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import Typography from '@/components/Typography';
+import Close from '@/assets/close.svg';
+
+export default {
+  components: {
+    Typography,
+    Close
+  },
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@use '@/styles/colors';
+@use '@/styles/units';
+
+.message {
+  --message__color: #{colors.css-color('grey')};
+  --message__background: transparent;
+  display: block;
+  position: relative;
+
+  &__close {
+    position: absolute;
+    top: 0;
+    right: 0;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+  }
+
+  &__body {
+    padding: units.spacing(10) units.spacing(12) units.spacing(10) units.spacing(12);
+  }
+
+  border: 1px solid var(--message__color);
+  background-color: var(--message__background);
+}
+
+</style>
