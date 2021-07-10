454eb57024f0c313fdb3cf46ab6336a0354e1153
Henry T <henry@henrytu.me>
2021-07-09 22:44:52 -0400

    Added Sentry Error Reporting

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue';
 import router from './router';
 import App from './App.vue';
+import * as Sentry from "@sentry/vue";
+import { Vue as VueIntegration } from "@sentry/integrations";
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+if (process.env.VUE_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    integrations: [new VueIntegration({
+      Vue: app
+    })],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
+app.use(router).mount('#app');
