e6d65048fe67aca68f976f0bd0ebd1a56dbc30f3
fpunny <frederic.pun@gmail.com>
2021-07-31 15:13:15 -0400

    fix: text and setup of acceptance

--- src/utils/useUserInfo.js
+++ src/utils/useUserInfo.js
@@ -1,12 +1,12 @@
 import { useStore } from 'vuex';
 import { computed } from 'vue';
 
-export default async function useUserInfo() {
+export default function useUserInfo() {
   const store = useStore();
   const userInfo = computed(() => store.state.userInfo);
 
   if (!userInfo.value) {
-    await store.dispatch('getUserInfo');
+    store.dispatch('getUserInfo');
   }
 
   return userInfo;
