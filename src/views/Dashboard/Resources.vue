75258b81ec09078689599b86987cad6ad3437b31
JennyVong <jennyvong502@gmail.com>
2021-07-31 21:48:18 -0400

    hackerinfo and resources page

--- src/views/Dashboard/Resources.vue
+++ src/views/Dashboard/Resources.vue
@@ -1,3 +1,64 @@
 <template>
-  <div>Resources</div>
+  <FormSection class="resources">
+      <Typography
+        class="resources__title"
+        color="dark-navy"
+        transform="uppercase"
+        type="heading3"
+        as="a"
+      >
+        Notion
+      </Typography>
+      <div class="resources__iframe">
+      <iframe 
+        class="resources__notion"
+        src="https://hackthe6ix2021.notion.site/hackthe6ix2021/Hack-the-6ix-Hacker-Guide-12ce5072583f487cac5c6e2bb128b599"
+      />
+      </div>
+  </FormSection>
 </template>
+
+<script>
+import FormSection from "@/components/FormSection";
+import Typography from "@/components/Typography";
+
+export default ({
+  components: {
+    FormSection,
+    Typography
+  }
+})
+</script>
+
+<style lang="scss">
+@use '@/styles/mixins';
+@use '@/styles/colors';
+@use '@/styles/units';
+
+.resources {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+
+  &__title {
+    margin-bottom: units.spacing(8);
+  }
+
+  &__iframe {
+    overflow: hidden;
+    padding-top: 100%;
+    position: relative;
+  }
+
+  &__notion {
+    border: none;
+    border-radius: 20px;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+}
+
+</style>
