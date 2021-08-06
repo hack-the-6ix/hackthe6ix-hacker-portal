bdadf038747dde03bfdd39acf510736953015117
fpunny <frederic.pun@gmail.com>
2021-08-05 22:12:31 -0400

    fixes

--- src/views/Sandbox.vue
+++ src/views/Sandbox.vue
@@ -1,24 +1,25 @@
 <template>
-  <iframe
-    class="sandbox__notion"
-    allowtransparency
-    frameborder="0"
-    src="https://ht6-notion.netlify.app/12ce5072583f487cac5c6e2bb128b599"
-  />
+  <div class="owo">
+    <Resources/>
+  </div>
 </template>
+
+<script>
+import Resources from '@/views/Dashboard/Resources';
+
+export default {
+  components: {
+    Resources,
+  },
+}
+</script>
 
 <style lang="scss">
 @use '@/styles/mixins';
 @use '@/styles/colors';
 @use '@/styles/units';
 
-.sandbox {
-  &__notion {
-    background: colors.css-color(gray);
-    border-radius: units.spacing(3);
-    margin-top: units.spacing(5);
-    height: units.spacing(200);
-    width: 100%;
-  }
+.owo {
+  background-color: white;
 }
 </style>
