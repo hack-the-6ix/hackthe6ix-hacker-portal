17a5654c45c61316a11e84f0abe5e95365825451
fpunny <frederic.pun@gmail.com>
2021-08-19 23:56:29 -0400

    task: Added localization and adjusted width of stuff

--- src/views/Dashboard/MailInfo.vue
+++ src/views/Dashboard/MailInfo.vue
@@ -1,15 +1,94 @@
 <template>
-    <div class="mail-info">
-      <div class="mail-info__content">
-        <Typography
-          class="mail-info__heading"
-          color="dark-navy"
-          type="heading3"
-          as="h1"
-        >
-          Mailing Address
-        </Typography>
+  <div class="mail-info">
+    <div class="mail-info__content">
+      <Typography
+        class="mail-info__heading"
+        color="dark-navy"
+        type="heading3"
+        as="h1"
+      >
+        Mailing Address
+      </Typography>
 
+      <template v-if="userInfo?.hackerApplication?.wantSwag">
+        <Typography
+          class="mail-info__description"
+          color="black"
+          type="paragraph"
+          as="p"
+        >
+          You have indicated that you are interested in receiving swag. You may
+          review the mailing address that you provided during the application
+          process below.
+        </Typography>
+        <Typography
+          class="mail-info__name"
+          color="black"
+          weight="700"
+          type="pararaph"
+          as="p"
+        >
+          {{ userInfo.firstName }} {{ userInfo.lastName }}
+        </Typography>
+        <Typography
+          class="mail-info__addressLine1"
+          color="black"
+          weight="700"
+          type="pararaph"
+          as="p"
+        >
+          {{ userInfo?.hackerApplication?.addressLine1 }}
+        </Typography>
+        <template v-if="userInfo.addressLine2">
+          <Typography
+            class="mail-info__addressLine2"
+            color="black"
+            weight="700"
+            type="pararaph"
+            as="p"
+          >
+            {{ userInfo?.hackerApplication?.addressLine2 }}
+          </Typography>
+        </template>
+        <Typography
+          class="mail-info__area"
+          color="black"
+          weight="700"
+          type="pararaph"
+          as="p"
+        >
+          {{ userInfo?.hackerApplication?.city }},
+          {{ userInfo?.hackerApplication?.province }}
+          {{ userInfo?.hackerApplication?.postalCode }}
+        </Typography>
+        <Typography
+          class="mail-info__country"
+          color="black"
+          transform="uppercase"
+          weight="700"
+          type="pararaph"
+          as="p"
+        >
+          {{ userInfo?.hackerApplication?.country }}
+        </Typography>
+      </template>
+      <template v-else>
+        <Typography
+          class="mail-info__description"
+          color="black"
+          type="paragraph"
+          as="p"
+        >
+          You have indicated that you are either not located in Canada, or do
+          not want swag. If you believe this is a mistake, please send us an
+          email at
+          <a class="mail-info__link" href="mailto:hello@hackthe6ix.com"
+            >hello@hackthe6ix.com</a
+          >.
+        </Typography>
+      </template>
+    </div>
+  </div>
         <template v-if="userInfo?.hackerApplication?.wantSwag">
 
           <Typography
@@ -99,16 +178,16 @@
 import Typography from '@/components/Typography';
 import useUserInfo from '@/utils/useUserInfo';
 
-export default ({
-    components: {
-        Typography,
-    },
-    setup() {
-      return {
-        ...useUserInfo(),
-      };
-    },
-})
+export default {
+  components: {
+    Typography,
+  },
+  setup() {
+    return {
+      ...useUserInfo(),
+    };
+  },
+};
 </script>
 
 <style lang="scss">
